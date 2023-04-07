a1a804c44f5d190a9d032dac2b1257ce3316f766
Reda Ghouzraf <rghouzra@e2r1p5.1337.ma>
2023-04-07 08:42:16 +0000

    add git ignore

--- philo/philosophers.c
+++ philo/philosophers.c
@@ -4,34 +4,44 @@ int struct_init(t_philos_table *table, t_params arg)
 {
 	if(!table)
 		return 0;
-	table->philos = malloc(arg.nb_philos * sizeof(t_philo));
-	
+	pthread_mutex_init(table->left_fork, NULL);
+	pthread_mutex_init(table->right_fork, NULL);
 	if(!table->philos)
 		return 0;
 	return 1;
 }
 
+void philo_take_fork(t_philos_table *philo)
+{
+	printf("philo id %d take fork\n", philo->philos->id);
+	pthread_mutex_lock(philo->left_fork);
+	pthread_mutex_lock(philo->right_fork);
+}
+
+void philo_eat(t_philos_table *philo)
+{
+	printf("philo id %d eat\n", philo->philos->id);
+	pthread_mutex_unlock(philo->left_fork);
+	pthread_mutex_unlock(philo->right_fork);
+}
+
+void philo_sleep(t_philos_table *philo)
+{
+	printf("philo id %d sleep\n", philo->philos->id);
+	usleep(philo->params.time_to_sleep);
+}
+
 void *philosophy(void *philosopher)
 {
-	t_philo *philo = (t_philo *)philosopher;
+	t_philos_table *philo = (t_philos_table *)philosopher;
 
 	while (1)
 	{
-		//think
-		//
-		//take forks
-		//	//if (philo->id % 2 == 0)
-		//	//	pthread_mutex_lock(philo->left_fork);	
-		//	//else
-		//	//	pthread_mutex_lock(philo->right_fork);
-		//	//pthread_mutex_lock(philo->left_fork);
-
-
-		//eat
-		//sleep
+		philo_take_fork(philo);
+		philo_eat(philo);
+		philo_sleep(philo);
 	}
 	return NULL;
-
 }
 
 void philosophy_start(t_philos_table *table)
@@ -40,14 +50,18 @@ void philosophy_start(t_philos_table *table)
 	int			i;
 
 	philos_thread = malloc(table->params.nb_philos * sizeof(pthread_t));
+	table->philos = malloc(sizeof(t_philo));
 	if (!philos_thread)
 		return ;
 	i = -1;
 	while (++i < table->params.nb_philos)
-		pthread_create(&philos_thread[i], NULL, &philosophy, &table->philos[i]);
+	{
+		table->philos->id = i;
+		pthread_create(&philos_thread[i], NULL, &philosophy, table);
+	}
 	i = -1;
 	while (++i < table->params.nb_philos)
-		pthread_join(philos_thread[i+1], NULL);
+		pthread_detach(philos_thread[i]);
 }
 
 void	prepare_table(t_params args)
@@ -55,6 +69,8 @@ void	prepare_table(t_params args)
 	t_philos_table	*table;
 
 	table = malloc(sizeof(t_philos_table));
+	table->left_fork = malloc(sizeof(pthread_mutex_t));
+	table->right_fork = malloc(sizeof(pthread_mutex_t));
 	if (!struct_init(table, args))
 		return ;
 	table->params = args;
--- philo/philosophers.h
+++ philo/philosophers.h
@@ -18,14 +18,16 @@ typedef struct s_params
 }t_params;
 typedef struct s_philo
 {
-	pthread_t *philos;
+	pthread_t	*philo;
+	int			id;
 }t_philo;
 typedef struct s_philos_table
 {
+	int id;
 	t_params		params;
-	pthread_mutex_t	*left_fork;
-	pthread_mutex_t	*right_fork;
 	t_philo			*philos;
+	pthread_mutex_t	*right_fork;
+	pthread_mutex_t	*left_fork;
 }t_philos_table;
 long	ft_atoi(const char *nptr);
 void	prepare_table(t_params args);
