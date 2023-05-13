f9b401c0a8274e56d626a9194617b90e3857d0aa
reda ghouzraf <redaghouzraf@MacBook-Pro-de-rghouzra.local>
2023-05-13 20:27:09 +0100

    add: routine of philos

--- philo_bonus/routine.c
+++ philo_bonus/routine.c
@@ -2,11 +2,13 @@
 
 void philo_take_fork(t_philo	*philo)
 {
+	__lock_print("has taken a fork", philo->id, philo);
+	__lock_print("has taken a fork", philo->id, philo);
 }
 
 void philo_eat(t_philo *philo)
 {
-
+	__lock_print("is eating", philo->id, philo);
 }
 
 void philo_sleep(t_philo *philo)
@@ -18,11 +20,15 @@ void philo_sleep(t_philo *philo)
 void philo_think(t_philo *philo)
 {
 	__lock_print("is thinking", philo->id, philo);
-	ft_usleep(200);
 }
 
 void philosopher_routine(t_philo *philo)
 {
-	printf("id->%d\tpid->%d\n", philo->id, philo->pid);
-
+	for(int i = 0; i < 100; i++)
+	{
+		philo_eat(philo);
+		philo_sleep(philo);
+		philo_think(philo);
+	}
+	exit(0);
 }
